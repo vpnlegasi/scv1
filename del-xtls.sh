702cbef3439535cfaf6f4de82eeb84176da97bd5
vpnlegasi <97322084+vpnlegasi@users.noreply.github.com>
2022-01-22 17:14:03 +0800

    Add files via upload

--- del-xtls.sh
+++ del-xtls.sh
@@ -13,32 +13,35 @@ echo "Only For Premium Users | Contact me at Telegram @vpnlegasi to Unlock"
 exit 0
 fi
 clear
-# // Getting V2Ray Client Data
-CLIENT_001=$(grep -c -E "^### " "/etc/xray-mini/config.json")
-echo "   
-CLIENT_002=$(grep -E "^### " "/etc/xray-mini/config.json" | cut -d ' ' -f 3 ') ==============================================="
-echo "            LIST VLESS CLIENT ON THIS VPS"
-echo "    ==============================================="
-grep -E "^### " "/etc/xray-mini/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
+
+# // Getting XRay Client Data
+CLIENT_001=$(grep -c -E "^### " "/etc/xray-mini/vless-nontls.json")
+echo "    =================================================="
+echo "               LIST VLESS CLIENT ON THIS VPS"
+echo "    =================================================="
+grep -e "^### " "/etc/xray-mini/config.json" | cut -d ' ' -f 2-8 | nl -s ') '
 	until [[ ${CLIENT_002} -ge 1 && ${CLIENT_002} -le ${CLIENT_001} ]]; do
 		if [[ ${CLIENT_002} == '1' ]]; then
-                echo "    ==============================================="
+                echo "    =================================================="
 			read -rp "    Please Input an Client Number (1-${CLIENT_001}) : " CLIENT_002
 		else
-                echo "    ==============================================="
+                echo "    =================================================="
 			read -rp "    Please Input an Client Number (1-${CLIENT_001}) : " CLIENT_002
 		fi
 	done
 
 # // String For Username && Expired Date
-client=$(grep -E "^### " "/etc/xray-mini/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_002}"p)
-expired=$(grep -E "^### " "/etc/xray-mini/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_002}"p)
+client=$(grep "^### " "/etc/xray-mini/config.json" | cut -d ' ' -f 4 | sed -n "${CLIENT_002}"p)
+expired=$(grep "^### " "/etc/xray-mini/config.json" | cut -d ' ' -f 8 | sed -n "${CLIENT_002}"p)
 
 # // Removing Data From Server Configuration
-sed -i "/^### $user $exp/,/^},{/d" /etc/xray-mini/config.json
+sed -i "/^### Username : $client | Expired : $expired/,/^### Username : $client | Expired : $expired/d" /etc/xray-mini/vless-nontls.json
+sed -i "/^### Username : $client | Expired : $expired/,/^### Username : $client | Expired : $expired/d" /etc/xray-mini/vless-tls.json
 
 # // Restarting XRay Service
+systemctl daemon-reload
 systemctl restart xray-mini
+
 
 # // Clear
 clear

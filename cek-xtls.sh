beb332420601db5a35cf888bcd27573e8efbe991
vpnlegasi <97322084+vpnlegasi@users.noreply.github.com>
2022-01-23 05:38:01 +0800

    Add files via upload

--- cek-xtls.sh
+++ cek-xtls.sh
@@ -4,39 +4,48 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ifconfig.me/ip);
 echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/vpnlegasi/ipv1.0/main/access | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${red}Permission Denied!${NC}";
+echo "Only For Premium Users | Contact me at Telegram @vpnlegasi to Unlock"
+exit 0
+fi
+clear
 echo -n > /tmp/other.txt
-data=( `cat /etc/xray-mini/config.json | grep '^#DEPAN' | cut -d ' ' -f 4`);
+data=( `cat /etc/xray-mini/config.json | grep '^###' | cut -d ' ' -f 2`);
 echo "-------------------------------";
-echo "Name : XRAY Vless User Login";
+echo "Name : XRAY XTLS User Login";
 echo "-------------------------------";
 for akun in "${data[@]}"
 do
 if [[ -z "$akun" ]]; then
 akun="tidak ada"
 fi
-echo -n > /tmp/ipvless.txt
+echo -n > /tmp/ipxtls.txt
 data2=( `netstat -anp | grep ESTABLISHED | grep tcp6 | grep xray | awk '{print $5}' | cut -d: -f1 | sort | uniq`);
 for ip in "${data2[@]}"
 do
 jum=$(cat /var/log/xray/access3.log | grep -w $akun | awk '{print $3}' | cut -d: -f1 | grep -w $ip | sort | uniq)
 if [[ "$jum" = "$ip" ]]; then
-echo "$jum" >> /tmp/ipvless.txt
+echo "$jum" >> /tmp/ipxtls.txt
 else
 echo "$ip" >> /tmp/other.txt
 fi
-jum2=$(cat /tmp/ipvless.txt)
+jum2=$(cat /tmp/ipxtls.txt)
 sed -i "/$jum2/d" /tmp/other.txt > /dev/null 2>&1
 done
-jum=$(cat /tmp/ipvless.txt)
+jum=$(cat /tmp/ipxtls.txt)
 if [[ -z "$jum" ]]; then
 echo > /dev/null
 else
-jum2=$(cat /tmp/ipvless.txt | nl)
+jum2=$(cat /tmp/ipxtls.txt | nl)
 echo "user : $akun";
 echo "$jum2";
 echo "-------------------------------"
 fi
-rm -rf /tmp/ipvmess.txt
+rm -rf /tmp/ipxtls.txt
 done
 oth=$(cat /tmp/other.txt | sort | uniq | nl)
 echo "other";

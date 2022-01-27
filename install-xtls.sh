ab832f3d2f13a385a8917b26763a778a989f4176
vpnlegasi <97322084+vpnlegasi@users.noreply.github.com>
2022-01-27 10:50:01 +0800

    Add files via upload

--- install-xtls.sh
+++ install-xtls.sh
@@ -55,6 +55,22 @@ LimitNOFILE=1000000
 WantedBy=multi-user.target
 END
 
+cat > /etc/systemd/system/xray-mini.service << END
+[Unit]
+Description=XRay-Mini
+Documentation=https://vpnlegasi.xyz
+After=syslog.target network-online.target
+[Service]
+User=root
+NoNewPrivileges=true
+ExecStart=/usr/local/xray-mini -c /etc/xray-mini/trojan.json
+Restart=on-failure
+RestartPreventExitStatus=23
+LimitNPROC=10000
+LimitNOFILE=1000000
+[Install]
+WantedBy=multi-user.target
+END
 # // XRay XTLS RPRX Direct
 cat > /etc/xray-mini/config.json << END
 {
@@ -63,169 +79,65 @@ cat > /etc/xray-mini/config.json << END
     "error": "/var/log/xray/error.log",
     "loglevel": "info"
   },
-    "inbounds": [
-        {
-            "port": 443,
-            "protocol": "vless",
-            "settings": {
-                "clients": [
-                    {
-                        "id" : "$uuid " ,
-                        "flow": "xtls-rprx-direct",
-                        "level": 0,
-                        "email": "ovpnlegasi@gmail.com"
-                    }
-                ],
-                "decryption": "none",
-                "fallbacks": [
-                    {
-                        "dest" : 1310 ,
-                        "xver": 1
-                    },
-                    {
-                        "path" : " /xray " ,
-                        "dest": 1234,
-                        "xver": 1
-                    },
-                    {
-                        "path" : " /xray " ,
-                        "dest": 2345,
-                        "xver": 1
-                    },
-                    {
-                        "path" : " /xray " ,
-                        "dest": 3456,
-                        "xver": 1
-                    }
-                ]
-            },
-            "streamSettings": {
-                "network": "tcp",
-                "security": "xtls",
-                "xtlsSettings": {
-                    "alpn": [
-                        "http/1.1"
-                    ],
-                    "certificates": [
-                        {
-                            "certificateFile": "${Cert_path}",
-                            "keyFile": "${Cert_Key_Path}"
-                        }
-                    ]
-                }
+  "inbounds": [
+    {
+      "port": ${port},
+      "protocol": "vless",
+      "settings": {
+        "clients": [
+          {
+            "id": "${uuid}",
+            "flow": "xtls-rprx-direct"
+#XRay
+          }
+        ],
+        "decryption": "none",
+        "fallbacks": [
+          {
+            "dest": 60000,
+            "alpn": "",
+            "xver": 1
+          },
+          {
+            "dest": 60001,
+            "alpn": "h2",
+            "xver": 1
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "tcp",
+        "security": "xtls",
+        "xtlsSettings": {
+          "minVersion": "1.2",
+          "certificates": [
+            {
+              "certificateFile": "${Cert_path}",
+              "keyFile": "${Cert_Key_Path}"
             }
-        },
-        {
-            "port": 1310,
-            "listen": "127.0.0.1",
-            "protocol": "trojan",
-            "settings": {
-                "clients": [
-                    {
-                        "password" : "$user " ,
-                        "level": 0,
-                        "email": "ovpnlegasi@gmail.com"
-                    }
-                ],
-                "fallbacks": [
-                    {
-                        "dest" : 2083
-                    }
-                ]
-            },
-            "streamSettings": {
-                "network": "tcp",
-                "security": "none",
-                "tcpSettings": {
-                    "acceptProxyProtocol": true
-                }
-            }
-        },
-        {
-            "port": 1234,
-            "listen": "127.0.0.1",
-            "protocol": "vless",
-            "settings": {
-                "clients": [
-                    {
-                        "id" : "$uuid " 
-                        "level": 0,
-                        "email": "ovpnlegasi@gmail.com"
-                    }
-                ],
-                "decryption": "none"
-            },
-            "streamSettings": {
-                "network": "ws",
-                "security": "none",
-                "wsSettings": {
-                    "path" : " /xray "
-                }
-            }
-        },
-        {
-            "port": 2345,
-            "listen": "127.0.0.1",
-            "protocol": "vmess",
-            "settings": {
-                "clients": [
-                    {
-                        "id" : "$uuid " ,
-                        "level": 0,
-                        "email": "ovpnlegasi@gmail.com"
-                    }
-                ]
-            },
-            "streamSettings": {
-                "network": "tcp",
-                "security": "none",
-                "tcpSettings": {
-                    "acceptProxyProtocol": true,
-                    "header": {
-                        "type": "http",
-                        "request": {
-                            "path": [
-                                " /xray" 
-                            ]
-                        }
-                    }
-                }
-            }
-        },
-        {
-            "port": 3456,
-            "listen": "127.0.0.1",
-            "protocol": "vmess",
-            "settings": {
-                "clients": [
-                    {
-                        "id" : "$uuid " ,
-                        "level": 0,
-                        "email": "ovpnlegasi@gmail.com"
-                    }
-                ]
-            },
-            "streamSettings": {
-                "network": "ws",
-                "security": "none",
-                "wsSettings": {
-                    "path" : " /xray "
-                }
-            }
+          ]
         }
-    ],
-    "outbounds": [
-        {
-            "protocol": "freedom"
-        }
-    ]
+      },
+      "sniffing": {
+        "enabled": true,
+        "destOverride": [
+          "http",
+          "tls"
+        ]
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
 }
 END
-# Config Trojan
 cat > /etc/xray-mini/trojan.json << END
 {
   "log": {
-    "access": "/var/log/xray/access3.log",
+    "access": "/var/log/xray/access4.log",
     "error": "/var/log/xray/error.log",
     "loglevel": "info"
     "inbounds": [
@@ -265,7 +177,6 @@ cat > /etc/xray-mini/trojan.json << END
 }
 END
 
-
 iptables -I INPUT -m state --state NEW -m udp -p udp --dport 443 -j ACCEPT
 iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
 iptables-save >/etc/iptables.rules.v4
@@ -288,7 +199,6 @@ wget -O renew-xtls "https://raw.githubusercontent.com/vpnlegasi/scv1/main/renew-
 wget -O cek-xtls "https://raw.githubusercontent.com/vpnlegasi/scv1/main/cek-xtls.sh"
 wget -O trial-xtls "https://raw.githubusercontent.com/vpnlegasi/scv1/main/trial-xtls.sh"
 wget -O xtls-exp "https://raw.githubusercontent.com/vpnlegasi/scv1/main/xtls-exp.sh"
-wget -O xray-tr "https://raw.githubusercontent.com/vpnlegasi/scv1/main/xray-tr.sh"
 
 chmod +x add-xtls
 chmod +x del-xtls
@@ -296,7 +206,6 @@ chmod +x renew-xtls
 chmod +x cek-xtls
 chmod +x trial-xtls
 chmod +x xtls-exp
-chmod +x xray-tr
 
 # // Remove Not Used Files
 rm -f install-xray.sh
